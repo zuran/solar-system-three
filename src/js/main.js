7daa0ad47e11ddf5ea57f3542e0e3b64a9252a1d
zuran <gsugrader@gmail.com>
2020-10-11 04:44:41 -0700

    orbiting planets at angle

--- src/js/main.js
+++ src/js/main.js
@@ -10,57 +10,66 @@ export default class Main {
   
     const fov = 45;
     const aspect = 2;  // the canvas default
-    const near = 0.1;
-    const far = 5;
+    const near = 0.01;
+    const far = 30;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2;
+    camera.position.z = .1;
+    camera.position.y = -.5;
+    camera.lookAt(0,0,0);
   
     const scene = new THREE.Scene();
   
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
-    //const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-  
-    const geometry = new THREE.SphereBufferGeometry(.1, 32, 32);
 
     {
       const color = 0xFFFFFF;
-      const intensity = 1;
-      const light = new THREE.DirectionalLight(color, intensity);
-      light.position.set(-1, 2, 4);
+      const intensity = .5;
+      const light = new THREE.PointLight(color, intensity);
+      light.position.set(0, 0, 0);
       scene.add(light);
+
+      const amblight = new THREE.AmbientLight(color, 0.3);
+      scene.add(amblight);
     }
 
-    const material = new THREE.MeshPhongMaterial({color: 0xFFFA00});  // greenish blue
-  
-    const sphere = new THREE.Mesh(geometry, material);
-    //scene.add(sphere);
-  
     renderer.render(scene, camera);
+
+    const geom = new THREE.SphereBufferGeometry(0.03, 64, 64);
+    const mate = new THREE.MeshBasicMaterial({
+      color: 0xccbb00,
+      //opacity: 0.5,
+      //transparent: true,
+    });
+    const sun = new THREE.Mesh(geom, mate);
+    scene.add(sun);
 
     function createPlanets() {
       // planetery scale multipliers - need to massage these to normalize the size somewhat
       const diameterScale = 0.0000005;
-      const distanceScale = 0.000235;
+      const distanceScale = 0.0008;
+      const periodScale = 0.002;
 
       let planets = [];
       // loop through bodies and add to scene;
       planetinfo.bodies.forEach(p => {
         if(p.name == 'Sun') return;
-        const geo = new THREE.SphereBufferGeometry(p.diameter * diameterScale, 16, 16);
+        const geo = new THREE.SphereBufferGeometry(p.diameter * diameterScale, 64, 64);
         const mat = new THREE.MeshPhongMaterial({color: p.color});
         const planet = new THREE.Mesh(geo, mat);
         // test distance and period scales
         planet.position.x = p.distance * distanceScale;
-        planet.period = p.period;
+        planet.distance = p.distance * distanceScale;
+        planet.period = p.period * periodScale;
         planets.push(planet);
         scene.add(planet);
       });
       // return body array
+      return planets;
     }
 
-    createPlanets();
+    const bodies = createPlanets();
 
     function render(time) {
       time *= 0.001;
@@ -71,11 +80,16 @@ export default class Main {
         camera.updateProjectionMatrix();
       }
 
-      sphere.position.x = Math.cos(time) * .5;
-      sphere.position.y = Math.sin(time) * .5;
+      // wiggle the sun
+      sun.position.x = Math.cos(time * 2) * .0003;
+      sun.position.y = Math.sin(time * 2) * .0003;
+      
 
-      // cube.rotation.x = time;
-      // cube.rotation.y = time;
+      bodies.forEach(planet => {
+        planet.position.x = Math.cos(time * 1/planet.period) * planet.distance;
+        planet.position.y = Math.sin(time * 1/planet.period) * planet.distance;
+      });
+
       renderer.render(scene, camera);
       requestAnimationFrame(render);
     }
