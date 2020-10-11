3745458e4d7084fc727d11610ae82b8910a4ab3a
zuran <gsugrader@gmail.com>
2020-10-11 03:36:33 -0700

    planets with some rough scales

--- src/js/main.js
+++ src/js/main.js
@@ -35,9 +35,32 @@ export default class Main {
     const material = new THREE.MeshPhongMaterial({color: 0xFFFA00});  // greenish blue
   
     const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
+    //scene.add(sphere);
   
     renderer.render(scene, camera);
+
+    function createPlanets() {
+      // planetery scale multipliers - need to massage these to normalize the size somewhat
+      const diameterScale = 0.0000005;
+      const distanceScale = 0.000235;
+
+      let planets = [];
+      // loop through bodies and add to scene;
+      planetinfo.bodies.forEach(p => {
+        if(p.name == 'Sun') return;
+        const geo = new THREE.SphereBufferGeometry(p.diameter * diameterScale, 16, 16);
+        const mat = new THREE.MeshPhongMaterial({color: p.color});
+        const planet = new THREE.Mesh(geo, mat);
+        // test distance and period scales
+        planet.position.x = p.distance * distanceScale;
+        planet.period = p.period;
+        planets.push(planet);
+        scene.add(planet);
+      });
+      // return body array
+    }
+
+    createPlanets();
 
     function render(time) {
       time *= 0.001;
