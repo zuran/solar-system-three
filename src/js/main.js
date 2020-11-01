92f8a91cac0ff26b0337d42b08474a1c651c5b1e
zuran <gsugrader@gmail.com>
2020-10-31 22:18:27 -0700

    moved texture import

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,14 @@
 import * as THREE from 'three';
 import planetinfo from '../data/planet-info.json5';
 import { VRButton } from '../resources/examples/jsm/webxr/VRButton.js';
-import jupiterImg from '../assets/2k_jupiter.jpg';
-import saturnImg from '../assets/2k_saturn.jpg';
-import mercuryImg from '../assets/2k_mercury.jpg';
-import venusImg from '../assets/2k_venus_surface.jpg';
-import earthImg from '../assets/2k_earth_daymap.jpg';
-import marsImg from '../assets/2k_mars.jpg';
-import neptuneImg from '../assets/2k_neptune.jpg';
-import uranusImg from '../assets/2k_uranus.jpg';
-import sunImg from '../assets/2k_sun.jpg';
-import plutoImg from '../assets/pluto.jpg';
 
-import saturnRingImg from '../assets/2k_saturn_ring_alpha.png';
-import skybox from '../assets/milkyway.jpg';
-
-import ganymedeImg from '../assets/ganymede_4k.jpg';
-import callistoImg from '../assets/callisto_4k.jpg';
-import europaImg from '../assets/europa_4k.jpg';
-import ioImg from '../assets/io_8k.jpg';
-import theMoonImg from '../assets/2k_moon.jpg';
-
-import dioneImg from '../assets/saturn/dione_rgb_cyl_www.jpg';
-import enceladusImg from '../assets/saturn/enceladus_rgb_cyl_www.jpg';
-import iapetusImg from '../assets/saturn/iapetus_rgb_cyl_www.jpg';
-import mimasImg from '../assets/saturn/mimas_rgb_cyl_www.jpg';
-import rheaImg from '../assets/saturn/rhea_rgb_cyl_www.jpg';
-import tethysImg from '../assets/saturn/tethys_rgb_cyl_www.jpg';
-import titanImg from '../assets/saturn/titan_texture_map_8k__2018_editon__by_fargetanik_dd05ce1-pre.jpg';
-
-import phobosImg from '../assets/2048_phobos.jpg';
-import deimosImg from '../assets/1024_deimos.jpg';
-
-import { Loader, MathUtils } from 'three';
+import { Loader, MathUtils, Texture } from 'three';
 import PlanetFactory from './planet-factory';
 import PickHelper from './pick-helper';
 import { divide, max, pick, update } from 'lodash';
 
 import * as moment from 'moment';
+import TextureImporter from './texture-import';
 
 export default class Main {
   constructor() {
@@ -295,82 +266,16 @@ export default class Main {
 
     const texLoader = new THREE.TextureLoader();
     // render skybox
-    const skytex = texLoader.load(skybox, () => {
-      const rt = new THREE.WebGLCubeRenderTarget(skytex.image.height);
-      rt.fromEquirectangularTexture(renderer, skytex);
-      scene.background = rt;
-    });
+    const skytex = texLoader.load(
+      TextureImporter.importTextures().skybox,
+      () => {
+        const rt = new THREE.WebGLCubeRenderTarget(skytex.image.height);
+        rt.fromEquirectangularTexture(renderer, skytex);
+        scene.background = rt;
+      }
+    );
 
-    const texMap = {
-      Sun: new THREE.MeshBasicMaterial({
-        color: 0xffc800,
-        map: texLoader.load(sunImg),
-      }),
-      Mercury: new THREE.MeshStandardMaterial({
-        map: texLoader.load(mercuryImg),
-      }),
-      Venus: new THREE.MeshStandardMaterial({ map: texLoader.load(venusImg) }),
-      Earth: new THREE.MeshStandardMaterial({ map: texLoader.load(earthImg) }),
-      Mars: new THREE.MeshStandardMaterial({ map: texLoader.load(marsImg) }),
-      Jupiter: new THREE.MeshStandardMaterial({
-        map: texLoader.load(jupiterImg),
-      }),
-      Saturn: new THREE.MeshStandardMaterial({
-        map: texLoader.load(saturnImg),
-      }),
-      Uranus: new THREE.MeshStandardMaterial({
-        map: texLoader.load(uranusImg),
-      }),
-      Neptune: new THREE.MeshStandardMaterial({
-        map: texLoader.load(neptuneImg),
-      }),
-      Ganymede: new THREE.MeshStandardMaterial({
-        map: texLoader.load(ganymedeImg),
-      }),
-      Callisto: new THREE.MeshStandardMaterial({
-        map: texLoader.load(callistoImg),
-      }),
-      Europa: new THREE.MeshStandardMaterial({
-        map: texLoader.load(europaImg),
-      }),
-      Io: new THREE.MeshStandardMaterial({
-        map: texLoader.load(ioImg),
-      }),
-      // Saturn moons
-      Dione: new THREE.MeshStandardMaterial({
-        map: texLoader.load(dioneImg),
-      }),
-      Enceladus: new THREE.MeshStandardMaterial({
-        map: texLoader.load(enceladusImg),
-      }),
-      Iapetus: new THREE.MeshStandardMaterial({
-        map: texLoader.load(iapetusImg),
-      }),
-      Mimas: new THREE.MeshStandardMaterial({
-        map: texLoader.load(mimasImg),
-      }),
-      Rhea: new THREE.MeshStandardMaterial({
-        map: texLoader.load(rheaImg),
-      }),
-      Tethys: new THREE.MeshStandardMaterial({
-        map: texLoader.load(tethysImg),
-      }),
-      Titan: new THREE.MeshStandardMaterial({
-        map: texLoader.load(titanImg),
-      }),
-      Deimos: new THREE.MeshStandardMaterial({
-        map: texLoader.load(deimosImg),
-      }),
-      Phobos: new THREE.MeshStandardMaterial({
-        map: texLoader.load(phobosImg),
-      }),
-      'The Moon': new THREE.MeshStandardMaterial({
-        map: texLoader.load(theMoonImg),
-      }),
-      Pluto: new THREE.MeshStandardMaterial({
-        map: texLoader.load(plutoImg),
-      }),
-    };
+    const texMap = TextureImporter.getTexMap();
 
     const scale = {
       diameter: 0.000002,
